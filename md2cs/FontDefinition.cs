57ae50b7207479e50542a5fbcf154bc5d269e4ec
Charlie Callow <charlie@linux.com>
2023-03-04 14:55:47 +0000

    Add summary comments to FontDefinition.cs

--- md2cs/FontDefinition.cs
+++ md2cs/FontDefinition.cs
@@ -1,11 +1,33 @@
 ﻿namespace md2cs
 {
+    /// <summary>
+    /// Defines a Material Design icon font.
+    /// </summary>
     public class FontDefinition
     {
+        /// <summary>
+        /// The name of the icon font.
+        /// </summary>
         public string FontName { get; set; }
+        
+        /// <summary>
+        /// The endpoint from which the codepoints file can be obtained.
+        /// </summary>
         public string CodePointsEndPoint { get; set; }
+        
+        /// <summary>
+        /// A composite format string for the icon URL containing a parameter for the icon name. 
+        /// </summary>
         public string FontUrlFormat { get; set; }
+        
+        /// <summary>
+        /// The URL to the page for this font.
+        /// </summary>
         public string SourceUrl { get; set; }
+        
+        /// <summary>
+        /// Returns the class name for this font.
+        /// </summary>
         public string ClassName => FontName?.Replace(" ", string.Empty);
     }
 }
